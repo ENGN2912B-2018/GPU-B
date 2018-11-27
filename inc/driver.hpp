9378fac53720816b87690dd42794a3c6959164d5
ChenZiwei <zwchen95@gmail.com>
2018-11-27 17:53:39 -0500

    Add test video I/O file

--- inc/driver.hpp
+++ inc/driver.hpp
@@ -1,18 +1,58 @@
 #ifndef _DRIVER_HPP
 #define _DRIVER_HPP
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+
+using namespace cv;
+using namespace std;
 
 namespace gpub
 {
 
-class Frame
-{
-};
+	class Frame{
+		/*
+		 * This class was designed to use OpenCL for video capture
+		 * openCV can be installed by $ brew install opencv
+		*/
+		public:
+			Frame(){}
 
-class FrameBuffer
-{
-public:
-	virtual Frame nextFrame();
-};
+			~Frame(){}
+
+			VideoCapture(){
+				// Initialize Video Stream Using OpenCV
+    			VideoCapture VideoStreamCap;
+				VideoStreamCap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'));// Video Stream Capture Settings
+    			VideoStreamCap.set(CV_CAP_PROP_FPS, 30);
+    			VideoStreamCap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
+    			VideoStreamCap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
+
+				// Initialize Video Capture GUI
+				VideoStreamCap.open(0);
+				namedWindow("Video Stream Capture");
+
+    			if( !VideoStreamCap.isOpened() ){
+        			std::cerr << "***Could not initialize capturing...***\n";
+        			return -1;
+    			}
+
+				//
+
+			}
+		
+
+		private:
+			cv::Mat VideoFrameMat; // Matrix for Video Frame Storeage
+			
+
+	};
+
+	class FrameBuffer{
+		public:
+			virtual Frame nextFrame();
+	};
 
 } // gpub
 
---
+++ test/OpenCVTest.cpp
@@ -0,0 +1,105 @@
+/******************************************************************************************
+ *File Name   : OpenCVTest.cpp
+ * Author      : Ziwei Chen
+ * Date        : Nov-23-2018
+ * Modified    : Ziwei Chen
+ * Modify Date : Nov-27-2018
+ * 
+ * Description : This program was designed to test the video capture
+ *               using OpenCV and buffer. Hardware camera is required
+ *               for running this program. OpenCV needs to be installed.
+ * 
+ * 
+ * OpenCV Installation: 
+ * https://medium.com/@jaskaranvirdi/setting-up-opencv-and-c-development-environment-in-xcode-b6027728003
+ * Compile Using:
+ * $ g++ $(pkg-config --cflags --libs opencv) -std=c++11  OpenCVTest.cpp -o VideoCapTest
+ * 
+ * ******************************************************************************************
+*/
+
+#include <iostream>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    //****************************** Record Settings ******************************//
+    int DeviceNumber = 0;
+    int RecDuration = 300;
+    int FrameRate = 30;
+    int FrameSamples = FrameRate * RecDuration;
+    //***************************** Hardware Settings *****************************//
+    VideoCapture VideoStreamCap;
+    VideoStreamCap.open(DeviceNumber);
+    VideoStreamCap.set(CV_CAP_PROP_XI_FRAMERATE, FrameRate);
+    if( !VideoStreamCap.isOpened() )
+    {
+        std::cerr << "***Could not Initialize Camera...***\n";
+        return -1;
+    }
+    //****************************** Initializations ******************************//
+    Mat VideoFrame;                                         // Each Video Frame
+    vector<Mat> FrameBufferVec;                             // Buffer Storage
+    cout << endl;
+    cout << endl;
+    cout << "************************** VIDEO CAPTURE ***************************" << endl;
+    cout << " Device Connected: Camera " << DeviceNumber << endl;
+    cout << " Record Duration: " << RecDuration << " Seconds" << endl;
+    cout << " Frame Rate: " << FrameRate << " Frames/Second" << endl;
+    cout << " Total Frame: " << FrameSamples << endl;
+    cout << "********************************************************************" << endl;
+    cout << endl;
+    cout << "CAMERA READY" << endl;
+    cout << "Press ENTER to Start Recording" << endl;
+    getchar();
+    waitKey(1000);
+    cout << "Start in: " << endl;
+    cout << " 5" << endl;
+    waitKey(1000);
+    cout << " 4" << endl;
+    waitKey(1000);
+    cout << " 3" << endl;
+    waitKey(1000);
+    cout << " 2" << endl;
+    waitKey(1000);
+    cout << " 1" << endl;
+    waitKey(1000);
+    cout << " 0" << endl;
+
+    //****************************** Video Recording ******************************//
+    int RecFrameCount = 0;
+    while(1){
+        VideoStreamCap >> VideoFrame;                                                   // Capture Video Frame
+
+        if(!(VideoFrame.empty())){ FrameBufferVec.push_back(VideoFrame.clone()); }      // Store in buffer
+        waitKey(1);
+        if (RecFrameCount == FrameSamples - 1){
+            break;
+        } else {
+            RecFrameCount ++;
+            cout << "Frame " << RecFrameCount << " Stored in buffer" << endl;
+        }
+    }
+        cout << "Complete" << endl;
+        cout << "Buffer Size: " << FrameBufferVec.size() << " Frames" << endl;
+        cout << "Press Enter to Start Playback" << endl;
+        getchar();
+    //****************************** Video Playback ******************************//
+    for (int index = 0; index < FrameSamples; index++){
+
+        VideoFrame = FrameBufferVec.at(index);
+        if(!(VideoFrame.empty())){
+            cout << "Reading From Buffer, Current Frame: " << index << endl;
+            imshow("Video Capture", FrameBufferVec.at(index));                          // Read buffer and display
+        } 
+        waitKey(33);
+    }
+    FrameBufferVec.clear();
+    return 1;
+}
