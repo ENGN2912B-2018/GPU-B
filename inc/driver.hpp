a15c6ae516b85c98df83556f9331cf656ad1b107
ChenZiwei <zwchen95@gmail.com>
2018-11-29 01:56:10 -0500

    Update driver, change buffer data type

--- inc/driver.hpp
+++ inc/driver.hpp
@@ -18,12 +18,10 @@
 #define _DRIVER_HPP
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/tracking.hpp>
-
-using namespace cv;
-using namespace std;
 
 namespace gpub
 {
@@ -35,128 +33,155 @@ namespace gpub
 		public:
 		/************************************ Con/Destructors ************************************/
 			Frame(){} 							// Constructor
-
 			~Frame(){} 							// Destructor
 		/************************************ Data Member Set ************************************/
-			void setDeviceNumber(int input) {DeviceNumber = input;}
-
-			void setFrameFate(int input) {FrameRate = input;}
-
-			void setRecDuration(int input) {RecDuration = input;}
-
-			void setOnScreenRec(bool input) { OnScreenRec = input;}
-		
-		    void resetBuffer() {
-				FrameBufferVec.clear();
-				ptBufferFrame = 0;
-				BufferFilled = false;
+			void setDeviceNumber(int input)
+			{
+				DeviceNumber = input;
 			}
 
-			void resetBufferPointer(){
-				ptBufferFrame = 0;
+			void setFrameFate(int input) 
+			{
+				FrameRate = input;
+			}
+
+			void setRecDuration(int input) 
+			{
+				RecDuration = input;
+			}
+
+			void setOnScreenRec(bool input) 
+			{
+				OnScreenRec = input;
 			}
 
 		/************************************ Data Member Get ************************************/
 
-			int getDeviceNumber() {return DeviceNumber;}
+			int getDeviceNumber() 
+			{
+				return DeviceNumber;
+			}
 
-			int getFrameRate() {return FrameRate;}
+			int getFrameRate() 
+			{
+				return FrameRate;
+			}
 
-			int getRecDuration() {return RecDuration;}
+			int getRecDuration( 
+			{
+				return RecDuration;
+			}
 
-			int getTotalFrame() {return FrameCount;}
+			int getTotalFrame() 
+			{
+				return FrameCount;
+			}
 
-			bool getDeviceStatus() {return DeviceEnable;} 
+			bool getDeviceStatus() 
+			{
+				return DeviceEnable;
+			} 
 
-			bool getBufferStatus() {return BufferFilled;}
+			bool getBufferStatus() 
+			{
+				return BufferFilled;
+			}
 
-			bool getOnScreenStatus() {return OnScreenRec;}
+			bool getOnScreenStatus() 
+			{
+				return OnScreenRec;
+			}
 
 		/************************************ Buffer Get *****************************************/
-			Mat frameGetFrame(){ 										// Take Current Frame and Move Pointer to Next
-				unsigned long long int thisFrame = ptBufferFrame;
-				if (thisFrame < RecFrameCount){
-					ptBufferFrame ++;
-					return FrameBufferVec.at(thisFrame);				// Return this frame
-				} else {
-					return emptyMat;									// Return emptyMat when out of range
+			cv::Mat get() 														// Take Current Frame and Move Pointer to Next
+			{
+				if (FrameBufferVec.size() > 0){
+					cv::Mat ThisFrame = FrameBufferVec.front(thisFrame);
+					FrameBufferVec.pop();
+					return ThisFrame;											// Return this frame
+				}
+				else 
+				{
+					return emptyMat;											// Return emptyMat when out of range
 				}
 			
 			}
 
-			Mat frameGetPrev(){ 										// Take Current Frame and Move Pointer to Previous
-				unsigned long long int thisFrame = ptBufferFrame;
-				if (thisFrame < RecFrameCount){ 						// Datatype for thisFrame: unsigned long long int, max 18,446,744,073,709,551,615
-					ptBufferFrame --;
-					return FrameBufferVec.at(thisFrame);
-				} else {
-					return emptyMat;									// Return emptyMat when out of range
+			cv::Mat peek() 														// Take Current Frame and Move Pointer to Previous
+			{
+				if (FrameBufferVec.size() > 0)
+				{
+					return FrameBufferVec.front(thisFrame);
+				}
+				else 
+				{
+					return emptyMat;											// Return emptyMat when out of range
 				}
 
 			}
 
-			Mat frameGetAny(unsigned long long int targetFrame){ 		// Get any frame
-				if (targetFrame < RecFrameCount){
-					return FrameBufferVec.at(targetFrame);				// Return this frame
-				} else {
-					return emptyMat;									// Return emptyMat when out of range
-				}
-			}
-
-			Mat framePeekNext(){ 										// Take Next Frame and keep pointer as it is
-				unsigned long long int thisFrame = ptBufferFrame + 1;
-				if (thisFrame < RecFrameCount){
-					return FrameBufferVec.at(thisFrame);				// Return this frame
-				} else {
-					return emptyMat;									// Return emptyMat when out of range
-				}
-			}
-
-			Mat framePeekPrev(){ 										// Take Previous Frame and keep pointer as it is
-				unsigned long long int thisFrame = ptBufferFrame - 1;
-				if (thisFrame < RecFrameCount){
-					return FrameBufferVec.at(thisFrame);				// Return this frame
-				} else {
-					return emptyMat;									// Return emptyMat when out of range
-				}
-			}
-
-			Mat framePeekThis(){ 										// Take Current Frame and keep pointer as it is
-				if (ptBufferFrame < RecFrameCount){
-					return FrameBufferVec.at(thisFrame);				// Return this frame
-				} else {
-					return emptyMat;									// Return emptyMat when out of range
-				}
-			}
 		/************************************ Video Record ***************************************/
-			void Video_Ini(){ 																	// Initialization for video
-				VideoStreamCap.open(DeviceNumber);												// Connect to camera
+			void video_ini()
+			{ 																		// Initialization for video
+				VideoStreamCap.open(DeviceNumber);									// Connect to camera
 				VideoStreamCap.set(CV_CAP_PROP_XI_FRAMERATE, FrameRate);
-				if(!VideoStreamCap.isOpened()){
+				if(!VideoStreamCap.isOpened())
+				{
 					DeviceEnable = true;
 				}
 			}
 
-			void Video_Capture(){ 																// Record Video （Option: Offscreen or Onscreed）
-				RecFrameCount = 0; 																// Reset Total Recorded Frame
-				if (OnScreenRec == true){
-					while RecFrameCount < FrameCount){
-						VideoStreamCap >> VideoFrame; 												// Capture Video Frame
-						if(!(VideoFrame.empty())) { FrameBufferVec.push_back(VideoFrame.clone()); } // Store in buffer
+			void video_capture()
+			{ 																		// Record Video （Option: Offscreen or Onscreed）
+				RecFrameCount = 0; 													// Reset Total Recorded Frame
+				if (OnScreenRec == true)
+				{
+					while RecFrameCount < FrameCount)
+					{
+						VideoStreamCap >> VideoFrame; 								// Capture Video Frame
+							if (FrameBufferVec.size() <= BufferSize)
+							{
+								FrameBufferVec.push(VideoFrame.clone()); 			// Store in buffer
+								imshow(VideoFrame);
+
+							}
+							else													// Keep buffer in size
+							{
+								FrameBufferVec.pop();
+								FrameBufferVec.push(VideoFrame.clone());
+								imshow(VideoFrame);
+							}
 					}
-				} else {
-					while RecFrameCount < FrameCount){
-						VideoStreamCap >> VideoFrame; 												// Capture Video Frame
+				}
+				else
+				{
+					while RecFrameCount < FrameCount)								// Capture Video Frame
+					{
+						VideoStreamCap >> VideoFrame; 					
 						imshow("Video Capture", VideoFrame);
-						if(!(VideoFrame.empty())) { FrameBufferVec.push_back(VideoFrame.clone()); } // Store in buffer
+						if(!(VideoFrame.empty()))
+						{	
+							if (FrameBufferVec.size() <= BufferSize)
+							{
+								FrameBufferVec.push(VideoFrame.clone()); 			// Store in buffer
+							}
+							else													// Keep buffer in size
+							{
+								FrameBufferVec.pop();
+								FrameBufferVec.push(VideoFrame.clone());
+							}
+						} 
 					}
 				}
 				BufferFilled = true;
 			}
 
-			void Video_Playback(){ 																// Playback Recorded Video in Buffer
-				for (int index = 0; index < FrameCount; index++){
-					imshow("Video Capture", FrameBufferVec.at(index));   						// Read Recorded buffer and playback
+			void video_playback()
+			{ 																		// Playback Recorded Video in Buffer
+				while (FrameBufferVec.size() > 0)
+				{
+					imshow("Video Capture", FrameBufferVec.front());   				// Read Recorded buffer and playback
+					FrameBufferVec.pop();
 				}
 			}
 
@@ -166,22 +191,22 @@ namespace gpub
 			bool DeviceEnable = false;
 			bool BufferFilled = false;
 
-			bool OnScreenRec = false;						// Show video while recording. Suggest set to false to save computing pwr
+			bool OnScreenRec = false;							// Show video while recording. Suggest set to false to save computing pwr
 
-			int DeviceNumber = 0;							// Device Number. Select when there are mutilpe cameras in system
-			int RecDuration = 5; 							// Video Clip duration in seconds
-			int FrameRate = 30; 							// Frame Rate in Frame/Second
-			int FrameCount = RecDuration * FrameRate; 		// Total Frame Number, 0 base
+			unsigned int DeviceNumber = 0;						// Device Number. Select when there are mutilpe cameras in system
+			unsigned int RecDuration = 5; 						// Video Clip duration in seconds
+			unsigned int FrameRate = 30; 						// Frame Rate in Frame/Second
+			unsigned int FrameCount = RecDuration * FrameRate; 	// Total Frame Number, 0 base
 
-			int RecFrameCount = 0;							// Total Frame Recorded, index, 0 base
+			unsigned int RecFrameCount = 0;						// Total Frame Recorded, index, 0 base
 
-			unsigned long long int ptBufferFrame = 0;       // Pointer for access buffer frames
+			unsigned int BufferSize = 150;                      // Defaule buffer size 5 seconds
 
-			Mat VideoFrame;									// For storing single frame
-			Mat emptyMat;                                 	// Empty matrix, for returning purpose
-			vector<Mat> FrameBufferVec;						// For frame buffer
+			cv::Mat VideoFrame;									// For storing single frame
+			cv::Mat emptyMat;                             		// Empty matrix, for returning purpose
+            std::queue <cv::Mat> FrameBufferVec;				// For frame buffer
 
-			VideoCapture VideoStreamCap;					// Camera Stream object
+			cv::VideoCapture VideoStreamCap;					// Camera Stream object
 
 	};
 
