c3695694df47dfdfb047a169c06c07da1f1a3180
ChenZiwei <zwchen95@gmail.com>
2018-11-30 00:11:00 -0500

    Update drive.cpp and split functions from driver.hpp

--- inc/driver.hpp
+++ inc/driver.hpp
@@ -2,8 +2,8 @@
  *File Name    : driver.hpp
  * Author      : Ziwei Chen
  * Date        : Nov-27-2018
- * Modified    : N/A
- * Modify Date : N/A
+ * Modified    : Ziwei Chen
+ * Modify Date : Nov-29-2017
  * 
  * Description : This class was to buffer the video stream from external device.
  * 
@@ -17,7 +17,7 @@
 #ifndef _DRIVER_HPP
 #define _DRIVER_HPP
 #include <iostream>
-#include <vector>
+#include <thread>
 #include <queue>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -31,188 +31,51 @@ namespace gpub
 		 * openCV can be installed by $ brew install opencv
 		*/
 		public:
-		/************************************ Con/Destructors ************************************/
-			Frame(){} 							// Constructor
-			~Frame(){} 							// Destructor
-		/************************************ Data Member Set ************************************/
-			void setDeviceNumber(int input)
-			{
-				DeviceNumber = input;
-			}
+			Frame(); 											// Constructor
+			~Frame(); 											// Destructor
 
-			void setFrameFate(int input) 
-			{
-				FrameRate = input;
-			}
+			void setDeviceNumber(int input);					// Set functions
+			void setFrameFate(int input); 
+			void setOnScreenRec(bool input); 
 
-			void setRecDuration(int input) 
-			{
-				RecDuration = input;
-			}
+			unsigned int getDeviceNumber(); 					// Get functions
+			unsigned int getFrameRate(); 
+			bool getDeviceStatus(); 
+			bool getOnScreenStatus(); 
 
-			void setOnScreenRec(bool input) 
-			{
-				OnScreenRec = input;
-			}
 
-		/************************************ Data Member Get ************************************/
-
-			int getDeviceNumber() 
-			{
-				return DeviceNumber;
-			}
-
-			int getFrameRate() 
-			{
-				return FrameRate;
-			}
-
-			int getRecDuration( 
-			{
-				return RecDuration;
-			}
-
-			int getTotalFrame() 
-			{
-				return FrameCount;
-			}
-
-			bool getDeviceStatus() 
-			{
-				return DeviceEnable;
-			} 
-
-			bool getBufferStatus() 
-			{
-				return BufferFilled;
-			}
-
-			bool getOnScreenStatus() 
-			{
-				return OnScreenRec;
-			}
-
-		/************************************ Buffer Get *****************************************/
-			cv::Mat get() 														// Take Current Frame and Move Pointer to Next
-			{
-				if (FrameBufferVec.size() > 0){
-					cv::Mat ThisFrame = FrameBufferVec.front(thisFrame);
-					FrameBufferVec.pop();
-					return ThisFrame;											// Return this frame
-				}
-				else 
-				{
-					return emptyMat;											// Return emptyMat when out of range
-				}
-			
-			}
-
-			cv::Mat peek() 														// Take Current Frame and Move Pointer to Previous
-			{
-				if (FrameBufferVec.size() > 0)
-				{
-					return FrameBufferVec.front(thisFrame);
-				}
-				else 
-				{
-					return emptyMat;											// Return emptyMat when out of range
-				}
-
-			}
-
-		/************************************ Video Record ***************************************/
-			void video_ini()
-			{ 																		// Initialization for video
-				VideoStreamCap.open(DeviceNumber);									// Connect to camera
-				VideoStreamCap.set(CV_CAP_PROP_XI_FRAMERATE, FrameRate);
-				if(!VideoStreamCap.isOpened())
-				{
-					DeviceEnable = true;
-				}
-			}
-
-			void video_capture()
-			{ 																		// Record Video （Option: Offscreen or Onscreed）
-				RecFrameCount = 0; 													// Reset Total Recorded Frame
-				if (OnScreenRec == true)
-				{
-					while RecFrameCount < FrameCount)
-					{
-						VideoStreamCap >> VideoFrame; 								// Capture Video Frame
-							if (FrameBufferVec.size() <= BufferSize)
-							{
-								FrameBufferVec.push(VideoFrame.clone()); 			// Store in buffer
-								imshow(VideoFrame);
-
-							}
-							else													// Keep buffer in size
-							{
-								FrameBufferVec.pop();
-								FrameBufferVec.push(VideoFrame.clone());
-								imshow(VideoFrame);
-							}
-					}
-				}
-				else
-				{
-					while RecFrameCount < FrameCount)								// Capture Video Frame
-					{
-						VideoStreamCap >> VideoFrame; 					
-						imshow("Video Capture", VideoFrame);
-						if(!(VideoFrame.empty()))
-						{	
-							if (FrameBufferVec.size() <= BufferSize)
-							{
-								FrameBufferVec.push(VideoFrame.clone()); 			// Store in buffer
-							}
-							else													// Keep buffer in size
-							{
-								FrameBufferVec.pop();
-								FrameBufferVec.push(VideoFrame.clone());
-							}
-						} 
-					}
-				}
-				BufferFilled = true;
-			}
-
-			void video_playback()
-			{ 																		// Playback Recorded Video in Buffer
-				while (FrameBufferVec.size() > 0)
-				{
-					imshow("Video Capture", FrameBufferVec.front());   				// Read Recorded buffer and playback
-					FrameBufferVec.pop();
-				}
-			}
-
-			
+			void video_ini();									// Record functions
+			void video_start(FrameBuffer& BufferObj)
+			void video_stop();
 
 		private:
 			bool DeviceEnable = false;
-			bool BufferFilled = false;
-
 			bool OnScreenRec = false;							// Show video while recording. Suggest set to false to save computing pwr
+			bool CaptureStart = false;
 
 			unsigned int DeviceNumber = 0;						// Device Number. Select when there are mutilpe cameras in system
-			unsigned int RecDuration = 5; 						// Video Clip duration in seconds
 			unsigned int FrameRate = 30; 						// Frame Rate in Frame/Second
-			unsigned int FrameCount = RecDuration * FrameRate; 	// Total Frame Number, 0 base
-
-			unsigned int RecFrameCount = 0;						// Total Frame Recorded, index, 0 base
-
-			unsigned int BufferSize = 150;                      // Defaule buffer size 5 seconds
 
 			cv::Mat VideoFrame;									// For storing single frame
-			cv::Mat emptyMat;                             		// Empty matrix, for returning purpose
-            std::queue <cv::Mat> FrameBufferVec;				// For frame buffer
-
 			cv::VideoCapture VideoStreamCap;					// Camera Stream object
-
 	};
 
 	class FrameBuffer{
 		public:
-			virtual Frame nextFrame();
+			FrameBuffer(); 										// Constructor
+			~FrameBuffer();  									// Destructor
+
+			void setSize(unsigned int inputSize); 
+			void push(const cv::Mat& inputFrame); 				// Store Function, pass by reference
+			
+			cv::Mat get();										// Get Frame Function
+			cv::Mat peek();
+
+		private:
+			unsigned int BufferSize = 150;
+			cd::Mat EmptyFrame;
+			std::queue <cd::Mat> FrameBuffer;
+
 	};
 
 } // gpub
--- src/drive.cpp
+++ src/drive.cpp
@@ -1,1 +1,149 @@
+/*
+ *File Name    : driver.cpp
+ * Author      : Ziwei Chen
+ * Date        : Nov-29-2018
+ * Modified    : N/A
+ * Modify Date : N/A
+ * 
+ * Description : This class was to buffer the video stream from external device.
+ * 
+ * OpenCV Installation: 
+ * https://medium.com/@jaskaranvirdi/setting-up-opencv-and-c-development-environment-in-xcode-b6027728003
+ * Compile Using (g++):
+ * $ g++ $(pkg-config --cflags --libs opencv) -std=c++11  OpenCVTest.cpp -o VideoCapTest
+ * 
+ * ******************************************************************************************
+*/
+
 #include "driver.hpp"
+
+/********************************** Video Capture Class *********************************/
+Frame::Frame(){}
+
+Frame::~Frame(){}
+
+// Set functions
+void Frame::setDeviceNumber(int input)
+{
+	DeviceNumber = input;
+}
+
+void Frame::setFrameFate(int input) 
+{
+	FrameRate = input;
+}
+
+void Frame::setOnScreenRec(bool input) 
+{
+	OnScreenRec = input;
+}
+
+// Get functions
+unsigned int Frame::getDeviceNumber() 
+{
+	return DeviceNumber;
+}
+
+unsigned int Frame::getFrameRate() 
+{
+	return FrameRate;
+}
+
+bool Frame::getDeviceStatus() 
+{
+	return DeviceEnable;
+} 
+
+bool Frame::getOnScreenStatus() 
+{
+	return OnScreenRec;
+}
+
+// Record functions
+void Frame::video_ini()
+{ 																		// Initialization for video
+	VideoStreamCap.open(DeviceNumber);									// Connect to camera
+	VideoStreamCap.set(CV_CAP_PROP_XI_FRAMERATE, FrameRate);
+	if(!VideoStreamCap.isOpened())
+	{
+		DeviceEnable = true;
+	}
+}
+
+
+void Frame::video_start(FrameBuffer& BufferObj)
+{ 																		// Record Video （Option: Offscreen or Onscreed）
+    CaptureStart = true;
+    while(CaptureStart == true)
+    {
+	    if (OnScreenRec == true)
+	    {
+		    VideoStreamCap >> VideoFrame; 						        // Capture Video Frame
+		    BufferObj.push(VideoFrame);                                 // Store in buffer
+		    imshow(VideoFrame);
+	    }
+	    else
+	    {
+		    VideoStreamCap >> VideoFrame;                               // Capture Video Frame
+		    BufferObj.push(VideoFrame);                                 // Store in buffer
+	    }
+    }
+}
+
+void Frame::video_stop()
+{
+    CaptureStart = false;
+}
+
+/********************************** Frame Buffer Class **********************************/
+
+FrameBuffer::FrameBuffer(){}
+
+FrameBuffer::~FrameBuffer(){}
+
+void FrameBuffer::setSize(unsigned int inputSize)                       // Set buffer size
+{
+    BufferSize = inputSize;
+}
+
+void FrameBuffer::push(const cv::Mat& inputFrame)                       // Push new frame into buffer
+{
+    if (FrameBuffer.size() < BufferSize)
+    {
+        FrameBuffer.push(inputFrame.clone());
+    }
+    else
+    {
+        FrameBuffer.pop();
+        FrameBuffer.push(inputFrame.clone());
+    }
+
+}
+
+cv::Mat FrameBuffer::get(){
+    if (FrameBuffer.size() > 0)
+    {
+        cv::Mat TempFrame = FrameBuffer.front();                        // Store the first frame
+        FrameBuffer.pop();                                              // Deleate the first frame
+
+        return TempFrame;                                               // Return the first frame
+    }
+    else
+    {
+        return EmptyFrame;
+    }
+}
+
+cv::Mat FrameBuffer::peek(){
+    if (FrameBuffer.size() > 0)
+    {
+        cv::Mat TempFrame = FrameBuffer.front();                        // Store the first frame
+        FrameBuffer.pop();                                              // Deleate the first frame
+
+        return TempFrame;                                               // Return the first frame
+    }
+    else
+    {
+        return EmptyFrame;
+    }
+}
