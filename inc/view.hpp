5981911ef1bcded651cc24321c17ca02f82faef9
Haoze Zhang <zhang@haoze.me>
2018-12-15 22:31:13 -0500

    Update: add img to GUI udpate function signature

--- inc/view.hpp
+++ inc/view.hpp
@@ -1,6 +1,7 @@
 #ifndef _VIEW_HPP
 #define _VIEW_HPP
 
+#include <opencv2/opencv.hpp>
 #include "logic.hpp"
 
 namespace gpub
@@ -9,7 +10,7 @@ namespace gpub
 class View
 {
 public:
-	void updateBallState(State);
+	void updateBallState(State state, cv::Mat img);
 };
 
 } // gpub
