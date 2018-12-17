171560fa1c0ac0562b5f2fcba6d428040ecee764
ChenZiwei <zwchen95@gmail.com>
2018-12-16 19:03:26 -0500

    Update main file and folder sturcture

--- app.cpp
+++ app.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <vector>
-#include "view.hpp"
-#include "logic.hpp"
-#include "driver.hpp"
+#include <QtWidgets>
+#include "./inc/view.hpp"
+#include "./inc/logic.hpp"
+#include "./inc/driver.hpp"
+#include "./inc/camera.h"
+//right before commit swap carriage in Git: look up in google
+// look for option for line ending
+// usability and user experience. Should we prompt the user otherwise?
 
 #define THREAD_NUM 4
 #define DEBUG
@@ -10,59 +15,63 @@
 /**
  * Main program entry
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	std::cout << "This is the ball tracker" << std::endl;
+    QApplication app(argc, argv);
 
-	gpub::HSVThreshold tracker;
-	gpub::FrameBuffer fb = gpub::CameraDriver();
-	gpub::View view;
+    Camera camera;
+    camera.show();
+    std::cout << "This is the ball tracker" << std::endl;
 
-	while (1)
-	{
-		std::vector<gpub::Frame> fv(THREAD_NUM);
-		std::vector<gpub::State> sv(THREAD_NUM);
-		for (int i=0; i<THREAD_NUM; i++)
-		{
-			fv[i] = fb.nextFrame();
-		}
-		#pragma omp parallel for num_threads(THREAD_NUM)
-		for (int i=0; i<THREAD_NUM; i++)
-		{
-			if (fv[i]) // if frame valid
-			{
-				try
-				{
-					sv[i] = tracker.analyse(fv[i]);
-				}
-				catch(const std::exception& e)
-				{
-					std::cerr << "Failed to analyse frame" << std::endl;
-				}
-			}
-			else
-			{
-				fb.start();
-			}
-		}
-		for (int i=0; i<THREAD_NUM; i++)
-		{
-			if (sv[i]) // if ball present
-			{
-				fb.stop();
-				try
-				{
-					view.updateBallState(sv[i], fv[i].getImg());
-					#ifdef DEBUG
-					std::cout << sv[i].getX() << " " << sv[i].getY() << std::endl;
-					#endif
-				}
-				catch(const std::exception& e)
-				{
-					std::cerr << "Failed to update GUI" << std::endl;
-				}
-			}
-		}
-	}
-	return 0;
+    gpub::HSVThreshold tracker;
+    gpub::FrameBuffer fb = gpub::CameraDriver();
+    gpub::View view;
+
+    while (1)
+    {
+        std::vector<gpub::Frame> fv(THREAD_NUM);
+        std::vector<gpub::State> sv(THREAD_NUM);
+        for (int i=0; i<THREAD_NUM; i++)
+        {
+            fv[i] = fb.nextFrame();
+        }
+#pragma omp parallel for num_threads(THREAD_NUM)
+        for (int i=0; i<THREAD_NUM; i++)
+        {
+            if (fv[i]) // if frame valid
+            {
+                try
+                {
+                    sv[i] = tracker.analyse(fv[i]);
+                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << "Failed to analyse frame" << std::endl;
+                }
+            }
+            else
+            {
+                fb.start();
+            }
+        }
+        for (int i=0; i<THREAD_NUM; i++)
+        {
+            if (sv[i]) // if ball present
+            {
+                fb.stop();
+                try
+                {
+                    view.updateBallState(sv[i], fv[i].getImg());
+#ifdef DEBUG
+                    std::cout << sv[i].getX() << " " << sv[i].getY() << std::endl;
+#endif
+                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << "Failed to update GUI" << std::endl;
+                }
+            }
+        }
+    }
+    return app.exec();
 }
